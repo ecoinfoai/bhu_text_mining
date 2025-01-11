05cfb949c2fa4e1d6d5fa75c14465f5e8dda4ea1
Kwang-Seuk <kjeong@bhug.ac.kr>
2025-01-11 15:30:06 +0900

    codelines for running functions removed

--- src/bhu_text_mining/naver_ocr.py
+++ src/bhu_text_mining/naver_ocr.py
@@ -198,22 +198,3 @@ def extract_text(responses: List[Dict]) -> Dict[str, str]:
             extracted_data[image_name] = aggregated_text
 
     return extracted_data
-
-
-# Initial settings
-config_path = "~/.config/naver_ocr/naver_ocr_config.json"
-image_file = [
-    "/home/kjeong/localgit/bhu_text_mining/data/fig01.jpg",
-    "/home/kjeong/localgit/bhu_text_mining/data/fig02.jpg",
-]
-
-secret_key, api_url = load_naver_ocr_env(config_path)
-
-request_json = create_request_json(image_file)
-print(request_json)
-
-response_data = send_images_receive_ocr(api_url, secret_key, image_file)
-print(response_data)
-
-extracted_data = extract_text(response_data)
-print(extracted_data)
